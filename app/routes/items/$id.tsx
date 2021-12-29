27279bee904664b7ae5ce3f6b9027502f3b3baed
o-hayato <preasper0+github@gmail.com>
2021-12-29 13:03:34 +0900

    add chart

--- app/routes/items/$id.tsx
+++ app/routes/items/$id.tsx
@@ -1,3 +1,16 @@
+import { format } from 'date-fns'
+import {
+  Area,
+  Brush,
+  CartesianGrid,
+  ComposedChart,
+  Legend,
+  Line,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis
+} from 'recharts'
 import type { LoaderFunction, MetaFunction } from 'remix'
 import { useLoaderData } from 'remix'
 
@@ -14,16 +27,77 @@ export const loader: LoaderFunction = async ({ params }) => {
     return { item: null }
   }
   const item = await getItemDetail(itemId)
-  console.log(item)
   return { item }
 }
 
-export default function ParamDemo() {
+export default function Items() {
   const data = useLoaderData<Props>()
+  const timeline = (data.item?.itemHistories ?? []).map(history => ({
+    ...history,
+    real: (history.price ?? 0) - (history.points ?? 0),
+    timestamp: format(new Date(history.scrapedAt * 1000), 'yyyy/MM/dd')
+  }))
   return (
-    <h1>
-      The param is <i style={{ color: 'red' }}>{JSON.stringify(data.item)}</i>
-    </h1>
+    <>
+      <h1>
+        <a href={data.item?.url}>{data.item?.title}</a>
+      </h1>
+      <ul>
+        <li>
+          更新日時：
+          {format(
+            new Date((data.item?.scrapedAt ?? 0) * 1000),
+            'yyyy/MM/dd HH:mm:ss'
+          )}
+        </li>
+      </ul>
+      <ResponsiveContainer aspect={16 / 9}>
+        <ComposedChart data={timeline}>
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="timestamp" />
+          <YAxis yAxisId={'left-yaxis'} />
+          <YAxis
+            yAxisId={'right-yaxis'}
+            unit={'%'}
+            orientation={'right'}
+            allowDecimals={false}
+          />
+          <Tooltip cursor={true} />
+          <Legend verticalAlign="top" wrapperStyle={{ lineHeight: '40px' }} />
+          <Brush
+            dataKey="timestamp"
+            stroke="#ffc658"
+            startIndex={timeline.length - 14}
+            endIndex={timeline.length - 1}
+          />
+          <Area
+            type="monotone"
+            stackId="1"
+            dataKey="real"
+            name={'実質価格'}
+            yAxisId={'left-yaxis'}
+            stroke="#ffc658"
+            fill="#ffc658"
+          />
+          <Line
+            type="monotone"
+            dataKey="discountRate"
+            stroke="#8884d8"
+            name={'値引率'}
+            unit={'%'}
+            yAxisId={'right-yaxis'}
+          />
+          <Line
+            type="monotone"
+            dataKey="pointsRate"
+            stroke="#82ca9d"
+            name={'ポイント率'}
+            unit={'%'}
+            yAxisId={'right-yaxis'}
+          />
+        </ComposedChart>
+      </ResponsiveContainer>
+    </>
   )
 }
 
